36e190a48e4beeb5dac5893a4597b5cef3b044fa
John Kiragu <kiragu@adalavice.co.ke>
2017-02-02 17:52:16 +0300

    Started working on Linear Regression and Gradient Descend

---
+++ src/main/java/ml/algorithms/GradientDescend.java
@@ -0,0 +1,12 @@
+
+package ml.algorithms;
+/**
+ *
+ * @author kiragu
+ */
+public class GradientDescend {
+
+    public static void main(String[] args) {
+        System.out.println("And the journey actually begins");
+    }
+}
---
+++ src/main/java/ml/algorithms/LinearRegression.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ml.algorithms;
+
+/**
+ * @author kiragu
+ */
+public class LinearRegression {
+ 
+    ///Meaning any progress made here
+    ///Can actually be used with this implementation
+    public static void main(String[] args) {
+        System.out.println("And the journey actually begins");
+    }
+}
